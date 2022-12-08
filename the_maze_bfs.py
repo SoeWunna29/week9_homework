53669374c52d5b1edc023b89d9981e97bf03b122
Soe Wunna <100667981+SoeWunna29@users.noreply.github.com>
2022-12-07 22:23:15 -0800

    Add files via upload

---
+++ the_maze_bfs.py
@@ -0,0 +1,35 @@
+class Solution:
+    def hasPath(self, maze, start, destination):
+
+        Q = [start]
+        n = len(maze)
+        m = len(maze[0])
+        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+        while Q:
+            i, j = Q.pop(0)
+            maze[i][j] = 2
+
+            if i == destination[0] and j == destination[1]:
+                print(len(Q))
+                return True
+
+            for x, y in dirs:
+                row = i + x
+                col = j + y
+                while 0 <= row < n and 0 <= col < m and maze[row][col] != 1:
+                    row += x
+                    col += y
+                row -= x
+                col -= y
+                if maze[row][col] == 0:
+                    Q.append([row, col])
+
+        return False
+
+
+maze = [[0, 0, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [1, 1, 0, 1, 1], [0, 0, 0, 0, 0]]
+start = [0, 4]
+destination = [2, 1]
+obj = Solution()
+print(obj.hasPath(maze, start, destination))
